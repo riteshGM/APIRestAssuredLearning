50ed6672233da43daae5b2dddcfacaf896b59907
Megha Sharma <megha2701@gmail.com>
2023-03-13 10:32:34 +0530

    First Commit

---
+++ src/test/java/org/restassured/scripts/SampleGetRequest.java
@@ -0,0 +1,24 @@
+package org.restassured.scripts;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class SampleGetRequest {
+	/**
+	 * Sample First GET Request Program
+	 */
+	@Test
+	public void getUsers() {
+		System.out.println("First RestAssured Program");
+		
+		given()
+		.when().get("https://reqres.in/api/users?page=2")
+		.then()
+		.assertThat().statusCode(200) //Checks if Status Code is 200
+		.log().all() // Prints Entire Response on Console
+		;
+		
+		
+	}
+
+}
---
+++ src/test/java/org/restassured/scripts/SamplePostRequest.java
@@ -0,0 +1,22 @@
+package org.restassured.scripts;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class SamplePostRequest {
+	
+	@Test
+	public void getUsers() {
+		System.out.println("First RestAssured Program");
+		
+		given()
+		.when().get("https://reqres.in/api/users?page=2")
+		.then()
+		.assertThat().statusCode(200) //Checks if Status Code is 200
+		.log().all() // Prints Entire Response on Console
+		;
+		
+		
+	}
+
+}
